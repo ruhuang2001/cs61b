67d43ab4a48fabe0069d0e8bcea40e77698065ba
ruhuang <ruhuang2001@gmail.com>
2021-09-13 22:48:39 +0800

    finish major proj0 Planet.java but have some bugs which need fix

--- README.md
+++ README.md
@@ -6,3 +6,6 @@ The course website : https://sp18.datastructur.es/
 
 [Lab1](https://github.com/ruhuang2001/cs61b/tree/master/lab1)(√)
 
+[Lab2](https://github.com/ruhuang2001/cs61b/tree/master/lab2)(√)
+
+[Proj0]() (todo)
---
+++ proj0/Planet.java
@@ -0,0 +1,83 @@
+public class Planet {
+    double xxPos;
+    double yyPos;
+    double xxVel;
+    double yyVel;
+    double mass; // weight
+    String imgFileName;
+
+    private static final double G = 6.67e-11;
+
+    public Planet(double xP, double yP, double xV, double yV, double m, String img) {
+        xxPos = xP;
+        yyPos = yP;
+        xxVel = xV;
+        yyVel = yV;
+        mass = m;
+        imgFileName =img;
+    }
+
+    public Planet(Planet p) {
+        this(p.xxPos, p.yyPos, p.xxVel, p.yyVel, p.mass, p.imgFileName);
+    }
+
+    public double calcDistance(Planet p) {
+        // return Math.sqrt(Math.pow(this.xxPos - other.xxPos, 2) + Math.pow(this.yyPos - other.yyPos, 2));
+        
+        double x = xxPos - p.xxPos;
+        double y = yyPos - p.yyPos;
+        return Math.sqrt(x * x + y * y);
+    }
+
+    public double calcForceExertedBy(Planet p) {
+        double distance = calcDistance(p);
+        return G * mass * p.mass / (distance * distance);
+    }
+
+    public double calcForceExertedByX(Planet p) {
+        double distance = calcDistance(p);
+        double force = calcForceExertedBy(p);
+        return (p.xxPos - xxPos) / distance * force;     
+    }
+
+    public double calcForceExertedByY(Planet p) {
+        double distance = calcDistance(p);
+        double force = calcForceExertedBy(p);
+        return (p.yyPos - yyPos) / distance * force;     
+    }
+
+    public double calcNetForceExertedByX(Planet[] p) {
+        double total = 0;
+        for (Planet each : p) {
+            if (this.equals(each)) 
+                continue;
+
+            total += calcForceExertedByX(each);
+        }
+        return total;
+    }
+
+    public double calcNetForceExertedByY(Planet[] p) {
+        double total = 0;
+        for (Planet each : p) {
+            if (this.equals(each))
+                continue;
+
+            total += calcForceExertedByY(each);
+        }
+        return total;
+    }
+
+    public void update(double duration, double xxForce, double yyForce) {
+        /*Acceleration */
+        double xA = xxForce / mass;
+        double yA = yyForce / mass;
+        double newxxVel = xxVel + duration * xA;
+        double newyyVel = yyVel + duration * yA;
+        xxVel = newxxVel;
+        yyVel = newyyVel;
+        xxPos = xxPos + duration * newxxVel;
+        yyPos = yyPos + duration * newxxVel;
+    }
+
+}
