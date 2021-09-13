687616aa8ef21d1decb67217f9341b8c34c72fcd
ruhuang <ruhuang2001@gmail.com>
2021-09-13 14:19:02 +0800

    completed lab1

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -1,7 +1,11 @@
-/** Class that determines whether or not a year is a leap year.
- *  @author YOUR NAME HERE
- */
 public class LeapYear {
+
+    /** Class that determines whether or not a year is a leap year.
+     *  @author ruhuang
+     */
+    public static boolean isLeapYear(int year) {
+        return (year%400 == 0) || (year%4==0 && year%100!=0);
+    }
 
     /** Calls isLeapYear to print correct statement.
      *  @param  year to be analyzed
