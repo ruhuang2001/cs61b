44c30144e052a03a1990c7beb907fc1c1c46342e
ruhuang <ruhuang2001@gmail.com>
2021-09-13 15:07:21 +0800

    fix hw1

---
+++ hw1/synthesizer/AbstractBoundedQueue.java
@@ -0,0 +1,10 @@
+package synthesizer;
+
+public abstract class AbstractBoundedQueue<T> implement BoundedQueue<T> {
+    protected int fillCount;
+    protected int capacity;
+    
+    protected void name() {
+        
+    }
+}
--- hw1/synthesizer/ArrayRingBuffer.java
+++ hw1/synthesizer/ArrayRingBuffer.java
@@ -1,10 +1,10 @@
 // TODO: Make sure to make this class a part of the synthesizer package
-// package <package name>;
+package <synthesizer>;
 import java.util.Iterator;
 
 //TODO: Make sure to make this class and all of its methods public
 //TODO: Make sure to make this class extend AbstractBoundedQueue<t>
-public class ArrayRingBuffer<T>  {
+public class ArrayRingBuffer<T> extend AbstractBoundedQueue<T>  {
     /* Index for the next dequeue or peek. */
     private int first;            // index for the next dequeue or peek
     /* Index for the next enqueue. */
@@ -21,6 +21,10 @@ public class ArrayRingBuffer<T>  {
         //       this.capacity should be set appropriately. Note that the local variable
         //       here shadows the field we inherit from AbstractBoundedQueue, so
         //       you'll need to use this.capacity to set the capacity.
+        first = 0;
+        last = 0;
+        fillCount = 0;
+        this.capacity = capacity;
     }
 
     /**
@@ -30,6 +34,8 @@ public class ArrayRingBuffer<T>  {
      */
     public void enqueue(T x) {
         // TODO: Enqueue the item. Don't forget to increase fillCount and update last.
+
+
     }
 
     /**
