ac3c8156d457ffdda60e0e2fcfcdfba240cb4220
ruhuang <ruhuang2001@gmail.com>
2021-09-19 17:56:56 +0800

    fix some bugs

--- proj1b/OffByN.java
+++ proj1b/OffByN.java
@@ -8,6 +8,6 @@ public class OffByN implements CharacterComparator {
 
 	@Override
 	public boolean equalChars(char x, char y) {
-		return (x-y) == N || (x-y) == -N;
+		return (x - y) == N || (x - y) == -N;
 	}
 }
--- proj1b/Palindrome.java
+++ proj1b/Palindrome.java
@@ -1,5 +1,5 @@
 public class Palindrome {
-	public Deque<Character> wordToDeque(String word){
+	public Deque<Character> wordToDeque(String word) {
 		Deque<Character> deque = new ArrayDeque<>();
 		for (int i = 0; i < word.length(); i++) {
 			deque.addLast(word.charAt(i));
@@ -9,29 +9,34 @@ public class Palindrome {
 
 	public boolean isPalindrome(String word) {
 		Deque<Character> deque = wordToDeque(word);
-		return isPalindrome(deque);
-	}
-
-	private boolean isPalindrome(Deque<Character> deque) {
-		if (deque.size() <= 1) {
-			return true;
-		}
-
-		if (deque.removeFirst() != deque.removeLast()) {
-			return false;
-		}
-
-		return isPalindrome(deque);
-	}
-
-	public boolean isPalindrome(String word, CharacterComparator cc) {
-		Deque<Character> deque = wordToDeque(word);
-		while(deque.size() > 1) {
-			if(!cc.equalChars(deque.removeFirst(), deque.removeLast())) {
+		while (Deque.size() > 1) {
+			if (deque.removeFirst() != deque.removeLast()) {
 				return false;
 			}
 		}
-		
+		return true;
+	}
+
+	// private boolean isPalindrome(Deque<Character> deque) {
+	// 	if (deque.size() <= 1) {
+	// 		return true;
+	// 	}
+
+	// 	if (deque.removeFirst() != deque.removeLast()) {
+	// 		return false;
+	// 	}
+
+	// 	return isPalindrome(deque);
+	// }
+
+	public boolean isPalindrome(String word, CharacterComparator cc) {
+		Deque<Character> deque = wordToDeque(word);
+		while (deque.size() > 1) {
+			if (!cc.equalChars(deque.removeFirst(), deque.removeLast())) {
+				return false;
+			}
+		}
+
 		return true;
 	}
 }
