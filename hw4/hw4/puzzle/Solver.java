1ee207f12d3ef8a06d26c52c55153430cbb6335d
ruhuang <ruhuang2001@gmail.com>
2021-12-02 23:09:20 +0800

    init HW4

---
+++ hw4/hw4/puzzle/Solver.java
@@ -0,0 +1,13 @@
+package hw4.puzzle;
+
+public class Solver {
+	public Solver(WorldState initial) {
+
+	}
+	public int moves() {
+		return 0;
+	}
+	public Iterable<WorldState> solution() {
+		return null;
+	}
+}
